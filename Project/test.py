664cabae762d8873a3e449b964114c32cc686280
Jaguo415 <83923903+Jaguo415@users.noreply.github.com>
2021-10-26 20:32:57 -0700

    Delete Project directory

--- Project/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-itemID = 560
-
-r = requests.get('http://services.runecape.com/m=itemdb_oldschool/api/catalogue/detail.json', params ={'item':itemID})
-json_data = json.loads(r.text)
-print(json_data)
