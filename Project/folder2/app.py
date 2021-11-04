5ad4a2253198beef1e16af6de6f941b0a42e212f
hdolci <huydo670@gmail.com>
2021-11-04 16:12:34 -0700

    adding working basic flask layout

--- Project/folder2/app.py
+++ Project/folder2/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+@app.route('/index')
+def index():
+    name = 'HD'
+    return render_template('index.html', title='Welcome', username=name)
